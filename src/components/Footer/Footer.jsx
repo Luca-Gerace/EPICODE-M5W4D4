58d48a7d939f0b63740ded0ad6089c2b8fe7c124
Luca-Gerace <geraceluca91@gmail.com>
2024-06-17 14:10:21 +0200

    update footer and notfound page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,17 @@
-import { useContext } from "react";
-import Context from "../../modules/Context";
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Header/units/Logo'
 
-function Footer() {
-  
-  // Theme prop
-  const { theme } = useContext(Context);
+export default function Footer() {
   
   return (
-    <footer className={`d-flex bg-theme-${theme} mt-4 py-4 shadow-lg`}>
-      <span className="m-auto p-2">
-        <strong>EPIBOOK</strong> - Copyright {new Date().getFullYear()}
-      </span>
+    <footer className='bg-transparent mt-4 py-4'>
+      <div className="d-flex flex-column m-auto p-2 border-top mt-4 pt-4 align-items-center justify-content-center">
+        <Link to='/' className='py-3'>
+          <Logo />
+        </Link>
+        <strong>EPIBOOKS</strong>
+        <span> Copyright {new Date().getFullYear()}</span>
+      </div>
     </footer>
   );
 }
-
-export default Footer;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import img404 from "../../public/404.svg"
+import { Button } from 'react-bootstrap'
+import { ChevronRight } from "react-bootstrap-icons";
 
 export default function NotFound() {
   return (
-    <>
-        <h1>Error 404</h1>
-        <p>page not found</p>
-        <Link to="/">Go back to homepage</Link>
-    </>
+    <div className="d-flex flex-column mb-4 pb-4 align-items-center justify-content-center w-75 m-auto">
+        <img src={img404} alt="Error 404" className="w-50" />
+        <h1 className="mb-4">Sorry, page not found</h1>
+        <Button as={Link} to="/" className='primaryBg w-auto px-4 d-flex justify-content-center align-items-center border-0 py-3'>
+          Go back to homepage
+          <ChevronRight  className='ms-2'/>
+        </Button>
+    </div>
   )
 }
